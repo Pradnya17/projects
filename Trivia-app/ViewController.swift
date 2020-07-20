a925d6a4e55285d9f9b34cd9c163611cd00868a7
Pradnya achari <pradnya.achari@humminwave.com>
2020-07-20 19:06:50 +0530

    Initial Commit

---
+++ Trivia-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia-app
+//
+//  Created by Pradnya Achari on 20/07/20.
+//  Copyright © 2020 Pradnya Achari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
